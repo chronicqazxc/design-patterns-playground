87d0c6426779bf817423bdc19564f06e033cd790
Wayne <xwayne@mc-cnjj-wdp024l.apac.wdpr.disney.com>
2017-03-26 15:12:53 +0800

    init bridge pattern

---
+++ DesignPatterns.playground/Pages/Bridge.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+/*:
+ [Previous](@previous)
+ 
+ ## Bridging
+ 
+ ### Intent
+
+ 
+ ![Design](Bridge_UML_class_diagram.png)
+ 
+ */
+
+import Foundation
+
+var str = "Hello, playground"
+
+/*:
+ [Next](@next)
+ - - -
+ This article uses material from the Wikipedia article ["Bridge pattern"], which is released under the [Creative Commons Attribution-Share-Alike License 3.0].
+ 
+ ["Bridge pattern"]: https://en.wikipedia.org/wiki/Bridge_pattern "Some hover text"
+ [Creative Commons Attribution-Share-Alike License 3.0]: https://creativecommons.org/licenses/by-sa/3.0/ ""
+ 
+ */
